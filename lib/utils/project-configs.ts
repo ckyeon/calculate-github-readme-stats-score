20d4277b13fb55f0a7076cfef2b16feeca9f0d8f
ckyeon <qsky8520@gmail.com>
2022-09-08 14:14:17 +0900

    Add github token, github fetch headers, github fetch total commit url

--- lib/utils/project-configs.ts
+++ lib/utils/project-configs.ts
@@ -1,3 +1,7 @@
+// Use an empty account for token
+
+export const GITHUB_TOKEN = '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';
+
 export const GITHUB_API_URL_BY_GRAPHQL = 'https://api.github.com/graphql';
 export const GITHUB_REQUEST_QUERY = `
       query userInfo($login: String!) {
@@ -33,8 +37,14 @@ export const GITHUB_REQUEST_QUERY = `
           }
         }
       }`;
-// Use an empty account for token
-export const GITHUB_TOKEN = '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';
+export const GITHUB_FETCH_STATS_HEADER = { Authorization: `bearer ${GITHUB_TOKEN}` };
+
+export const GITHUB_FETCH_TOTAL_COMMITS_URL = 'https://api.github.com/search/commits?q=author:';
+export const GITHUB_FETCH_TOTAL_COMMITS_HEADER = {
+  'Content-Type': 'application/json',
+  Accept: 'application/vnd.github.cloak-preview',
+  Authorization: `token ${GITHUB_TOKEN}`,
+};
 
 export const GET = 'get';
 export const POST = 'post';
